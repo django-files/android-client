5b1ccb9033f662871fc123571a031322488b4d57
Shane <6071159+smashedr@users.noreply.github.com>
2025-04-03 21:54:48 -0700

    Change to context?.getSharedPreferences

--- app/src/main/java/com/djangofiles/djangofiles/settings/SettingsFragment.kt
+++ app/src/main/java/com/djangofiles/djangofiles/settings/SettingsFragment.kt
@@ -2,10 +2,13 @@ package com.djangofiles.djangofiles.settings
 
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
 import androidx.core.content.edit
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
+//import androidx.preference.PreferenceManager
+import com.djangofiles.djangofiles.MainActivity.Companion.PREFS_NAME
+import com.djangofiles.djangofiles.MainActivity.Companion.URL_KEY
 import com.djangofiles.djangofiles.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -17,23 +20,24 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val savedUrlPref = findPreference<EditTextPreference>("saved_url")
         Log.d("SettingsFragment", "savedUrlPref: $savedUrlPref")
 
+        val preferences = context?.getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        Log.d("SettingsFragment", "preferences: $preferences")
+
         savedUrlPref?.let {
-            val savedUrl = PreferenceManager.getDefaultSharedPreferences(requireContext())
-                .getString("saved_url", "")
-
-            // Log the saved URL
-            Log.d("SettingsFragment", "Loaded saved URL: $savedUrl")
-
+            //val savedUrl = PreferenceManager.getDefaultSharedPreferences(requireContext())
+            //    .getString("saved_url", "")
+            val savedUrl = preferences?.getString(URL_KEY, "")
+            Log.d("SettingsFragment", "savedUrl: $savedUrl")
             it.text = savedUrl
         }
 
         savedUrlPref?.setOnPreferenceChangeListener { _, newValue ->
             val newUrl = newValue as String
-            // Log the updated URL
-            Log.d("SettingsFragment", "Updated saved URL: $newUrl")
+            Log.d("SettingsFragment", "newUrl: $newUrl")
 
-            PreferenceManager.getDefaultSharedPreferences(requireContext())
-                .edit { putString("saved_url", newUrl) }
+            //PreferenceManager.getDefaultSharedPreferences(requireContext())
+            //    .edit { putString("saved_url", newUrl) }
+            preferences?.edit { putString(URL_KEY, newUrl) }
             true
         }
     }
