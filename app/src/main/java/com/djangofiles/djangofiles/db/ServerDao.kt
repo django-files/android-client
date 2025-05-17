93a9ad8c7cd7f0da8777b9c2177091e5049a9367
Shane <6071159+smashedr@users.noreply.github.com>
2025-05-17 10:29:01 -0700

    Add Database Migration

--- app/src/main/java/com/djangofiles/djangofiles/db/ServerDao.kt
+++ app/src/main/java/com/djangofiles/djangofiles/db/ServerDao.kt
@@ -11,6 +11,8 @@ import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.Upsert
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 
 @Dao
@@ -61,6 +63,15 @@ abstract class ServerDatabase : RoomDatabase() {
         @Volatile
         private var instance: ServerDatabase? = null
 
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE Server ADD COLUMN size INTEGER")
+                database.execSQL("ALTER TABLE Server ADD COLUMN count INTEGER")
+                database.execSQL("ALTER TABLE Server ADD COLUMN shorts INTEGER")
+                database.execSQL("ALTER TABLE Server ADD COLUMN humanSize TEXT")
+            }
+        }
+
         fun getInstance(context: Context): ServerDatabase =
             instance ?: synchronized(this) {
                 instance ?: Room.databaseBuilder(
@@ -68,7 +79,8 @@ abstract class ServerDatabase : RoomDatabase() {
                     ServerDatabase::class.java,
                     "server-database"
                 )
-                    .fallbackToDestructiveMigration(true) // TODO: NOT THIS!!!
+                    //.fallbackToDestructiveMigration(true) // Destructive Operation
+                    .addMigrations(MIGRATION_1_2)
                     .build().also { instance = it }
             }
     }
