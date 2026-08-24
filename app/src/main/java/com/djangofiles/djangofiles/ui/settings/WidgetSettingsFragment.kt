092b4f6da13003f740747728d49ad41cebeb6214
Shane <6071159+smashedr@users.noreply.github.com>
2026-08-23 20:00:46 -0700

    Add Back/Header to Widget Settings

--- app/src/main/java/com/djangofiles/djangofiles/ui/settings/WidgetSettingsFragment.kt
+++ app/src/main/java/com/djangofiles/djangofiles/ui/settings/WidgetSettingsFragment.kt
@@ -2,17 +2,49 @@ package com.djangofiles.djangofiles.ui.settings
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import androidx.navigation.fragment.findNavController
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SeekBarPreference
 import com.djangofiles.djangofiles.R
+import com.djangofiles.djangofiles.databinding.FragmentWidgetSettingsBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class WidgetSettingsFragment : PreferenceFragmentCompat() {
+
+    // NOTE: The fragment_widget_settings.xml binding and onCreateView/onDestroyView
+    //       are all for the Back Button and Heading on the Widget Settings
+    private var _binding: FragmentWidgetSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    private val navController by lazy { findNavController() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        Log.d("WidgetSettingsFragment", "onCreateView: $savedInstanceState")
+        // Inflate a custom layout with a title/back header and nest the
+        // PreferenceFragmentCompat view (with its RecyclerView) inside of it.
+        _binding = FragmentWidgetSettingsBinding.inflate(inflater, container, false)
+        val prefsView = super.onCreateView(inflater, binding.preferenceContainer, savedInstanceState)
+        Log.d("WidgetSettingsFragment", "prefsView: $prefsView")
+        binding.preferenceContainer.addView(prefsView)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        Log.d("WidgetSettingsFragment", "onDestroyView")
+        super.onDestroyView()
+        _binding = null
+    }
 
     override fun onStart() {
         super.onStart()
@@ -33,6 +65,13 @@ class WidgetSettingsFragment : PreferenceFragmentCompat() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("WidgetSettingsFragment", "onViewCreated: $savedInstanceState")
+
+        binding.goBack.setOnClickListener {
+            Log.d("WidgetSettingsFragment", "binding.goBack: navController.navigateUp()")
+            navController.navigateUp()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
             val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             Log.d("ViewCompat", "top: ${bars.top}")
